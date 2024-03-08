0f3e74f791f4314cbff4a9154659369340be97d0
Gerhard Lausser <gerhard.lausser@consol.de>
2024-03-08 17:42:13 +0100

    add a default port to nsc runner

--- eventhandler/src/eventhandler/nsc_web/runner.py
+++ eventhandler/src/eventhandler/nsc_web/runner.py
@@ -1,3 +1,4 @@
+import os
 from eventhandler.baseclass import EventhandlerRunner
 
 class NscWebRunner(EventhandlerRunner):
@@ -5,15 +6,13 @@ class NscWebRunner(EventhandlerRunner):
     def __init__(self, opts):
         super(self.__class__, self).__init__(opts)
         setattr(self, "hostname", getattr(self, "hostname", "localhost"))
-        setattr(self, "port", getattr(self, "port", None))
+        setattr(self, "port", getattr(self, "port", 8443))
         setattr(self, "password", getattr(self, "password", None))
 
     def run(self, event):
-        cmd = "{}/lib/nagios/plugins/check_nsc_web -k -u "https://{}:{}" -p "{}" -t 180'.format(os.environ["OMD_ROOT"], self.hostname, self.port, self.password)
+        cmd = "{}/lib/nagios/plugins/check_nsc_web -k -u https://{}:{} -p '{}' -t 180".format(os.environ["OMD_ROOT"], self.hostname, self.port, self.password)
         if "arguments" in event.payload:
             cmd += " {} '{}'".format(event.payload["command"], event.payload["arguments"])
         else:
             cmd += " {}".format(event.payload["command"])
-        if self.port:
-            cmd += f" -p {self.port}"
-        logger.debug(cmd)
+        return cmd
