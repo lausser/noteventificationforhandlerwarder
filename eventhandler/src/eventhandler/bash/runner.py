807f3c80aea64fd3d41d90a5817ef60a8ccaffbb
Gerhard Lausser <lausser@yahoo.com>
2024-03-24 16:48:34 +0100

    fix bash command

--- eventhandler/src/eventhandler/bash/runner.py
+++ eventhandler/src/eventhandler/bash/runner.py
@@ -4,7 +4,9 @@ class BashRunner(EventhandlerRunner):
 
     def __init__(self, opts):
         super(self.__class__, self).__init__(opts)
+        setattr(self, "command", getattr(self, "command", "exit 0"))
+
 
     def run(self, event):
-        cmd = "bash -c '{}'".format(event.payload["command"])
+        cmd = "bash -c '{}'".format(self.command)
         return cmd
